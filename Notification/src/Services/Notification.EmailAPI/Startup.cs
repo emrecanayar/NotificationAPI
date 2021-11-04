b819383373a4500a6ae518c817a5bd173bae4508
muratvuranok <mrtvrnk@gmail.com>
2021-11-04 12:04:51 +0300

    publishEdermisin

--- Notification/src/Services/Notification.EmailAPI/Startup.cs
+++ Notification/src/Services/Notification.EmailAPI/Startup.cs
@@ -1,30 +1,22 @@
 ﻿using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Notification.Shared.Filters;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Notification.EmailAPI
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; } 
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
@@ -33,9 +25,7 @@ namespace Notification.EmailAPI
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Notification.EmailAPI", Version = "v1" });
             });
-           
-
-            //Default port => 5672
+            
             services.AddMassTransit(x =>
             {
                 x.UsingRabbitMq((context, cfg) =>
@@ -44,8 +34,7 @@ namespace Notification.EmailAPI
                     {
                         host.Username("guest");
                         host.Password("guest");
-                    });
-
+                    }); 
                     cfg.AutoDelete = true;
                 });
             });
@@ -53,21 +42,19 @@ namespace Notification.EmailAPI
             services.AddMassTransitHostedService();
             services.AddAutoMapper(typeof(Startup));
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+         
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Notification.EmailAPI v1"));
+                app.UseDeveloperExceptionPage(); 
             }
 
-            app.UseRouting();
 
-            app.UseAuthorization();
-
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Notification.EmailAPI v1"));
+            app.UseRouting(); 
+            app.UseAuthorization(); 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
